4ff4c49034ef6ab8a4190901d93127b6179c543c
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-10-04 16:54:07 +0300

    check work codeShip

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -19,7 +19,7 @@ public class Logic {
         this.figures[this.index++] = figure;
     }
 
-    public boolean move(Cell source, Cell dest) {
+    public boolean move(Cell source, Cell dest){
         boolean rst = false;
         int index = this.findBy(source);
         if (index != -1) {
